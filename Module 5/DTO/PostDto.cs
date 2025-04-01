0166e9ca30cf211af56a3c00a7af2e4f3ece6b75
bhoomi <bhoomi1414agarwal@gmail.com>
2025-04-02 00:30:45 +0530

    Changes in postDto

--- Module 5/DTO/PostDto.cs
+++ Module 5/DTO/PostDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Module_5.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Module_5.DTO
 {
     public class PostDto
     {
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content is required.")]
         public string Content { get; set; }
         public string? ImageUrl { get; set; }
        
