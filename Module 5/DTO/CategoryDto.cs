e448f09dac57b7a9aaff9838d01d87509915f96a
bhoomi <bhoomi1414agarwal@gmail.com>
2025-04-02 00:26:32 +0530

    Changes in DTO

--- Module 5/DTO/CategoryDto.cs
+++ Module 5/DTO/CategoryDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Module_5.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Module_5.DTO
 {
     public class CategoryDto
     {
+        [Required(ErrorMessage = "CategoryName is required.")]
         public string CategoryName { get; set; }
+
         public string Description { get; set; }
 
 
